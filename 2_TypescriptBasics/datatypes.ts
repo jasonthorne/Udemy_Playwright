4115dd2dbbed421efabc6b178dba41c8331dc941
jasonthorne <jasonthorne@live.ie>
2025-06-29 16:14:23 +0100

    variables

---
+++ 2_TypescriptBasics/datatypes.ts
@@ -0,0 +1,20 @@
+
+let myBoolean: boolean = true;
+console.log(myBoolean);
+
+let myNum: number = 333;
+console.log(myNum);
+
+let myString: string = "myString";
+console.log(myString);
+
+//any - can assign values of any type:
+let myAny: any = 333;
+console.log(myAny);
+myAny = "Im now a string";
+console.log(myAny);
+
+//null and undefined:
+let myNull: null = null; //used for intential abscence of a value
+let myUndefined: undefined = undefined; //unintential abcxense of a value
+console.log(myNull, myUndefined);
