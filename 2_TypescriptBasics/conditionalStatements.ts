13a64d6d28d3965d10e6ba488ed2ec09247475ef
jasonthorne <jasonthorne@live.ie>
2025-06-29 18:48:25 +0100

    switch

---
+++ 2_TypescriptBasics/conditionalStatements.ts
@@ -0,0 +1,8 @@
+
+let test1: number = 33;
+
+if(test1<18){
+    console.log('woohoo!');
+}else{
+    console.log('god dammit! :/')
+}
---
+++ 2_TypescriptBasics/switchStatements.ts
@@ -0,0 +1,14 @@
+
+let test2: number = 333;
+
+switch(test2){
+    case 111:
+        console.log('111');
+        break;
+    case 333:
+        console.log('333');
+        break;
+    default:
+        console.log('uh oh!');
+        break;
+}
