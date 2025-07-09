d81afb1e4c7873bf1ec5cccf784fa509afdbedf9
jasonthorne <jasonthorne@live.ie>
2025-07-09 23:07:36 +0100

    automatic playwright code gen

--- 4_playwright/tests/autoProducedCodeGenTest.spec.ts
+++ 4_playwright/tests/autoProducedCodeGenTest.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+
 test('test', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
   await page.getByRole('button', { name: 'Decline optional cookies' }).click();
