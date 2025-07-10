220a959c4e162bf003131a38c506a8e5c31a9293
jasonthorne <jasonthorne@live.ie>
2025-07-10 23:38:12 +0100

    traceviewer

--- 4_playwright/playwright.config.ts
+++ 4_playwright/playwright.config.ts
@@ -12,13 +12,15 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout:5*1000, //times out in 5 seconds instead of default 30 ++++++++++++++
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  ///////////////////++++++++++retries: process.env.CI ? 2 : 0,
+  retries: 1, //makes a test as fail after first fail (I think!) +++++++++++++++
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
--- 4_playwright/tests/codeGenTest.spec.ts
+++ 4_playwright/tests/codeGenTest.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
-  await page.getByRole('button', { name: 'Decline optional cookies' }).click();
+  //+++++++++++++++++ TEST DELIBERATELY FAILS HERE :P +++++
+  await page.getByRole('button', { name: 'this doesnt exist!' /*'Decline optional cookies'*/ }).click();
 });
