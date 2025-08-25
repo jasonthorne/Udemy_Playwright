a71b9e73b0334adc42f31f03aae9a5866cffdba1
jasonthorne <jasonthorne@live.ie>
2025-08-25 21:19:45 +0100

    fill method

--- 4_playwright/tests/textBoxHandling.spec.ts
+++ 4_playwright/tests/textBoxHandling.spec.ts
@@ -1,7 +1,20 @@
 import {test} from '@playwright/test';
 
-//Fill method - most common method for entering text into a txt box:
+//Fill method - most common method for entering text into a text box:
 test('Fill Method', async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.locator('input[placeholder="Username"]').fill('Admin');
+    await page.locator('input[placeholder="Password"]').fill('admin123');
+    ///await page.locator('input[placeholder="Password"]').press('Enter');
+    await page.locator('button[type="submit"]').click();
+    await page.locator('.oxd-userdropdown-tab').click();
+    await page.locator('text=Logout').click();
+    
+    await page.close();
+});
+
+
+test('Press: Sequentially Method', async({page})=>{
     await page.goto('https://opensource-demo.orangehrmlive.com');
     await page.locator('input[placeholder="Username"]').fill('Admin');
     await page.locator('input[placeholder="Password"]').fill('admin123');
