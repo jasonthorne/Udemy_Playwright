59d43445fda0504bcd7a7c543a0f7075a3eff84d
jasonthorne <jasonthorne@live.ie>
2025-08-27 19:56:09 +0100

    press sequentially with delay

--- 4_playwright/tests/textBoxHandling.spec.ts
+++ 4_playwright/tests/textBoxHandling.spec.ts
@@ -14,13 +14,26 @@ test/*.only*/('Fill Method', async({page})=>{
 });
 
 
-//press sequentially - enters each letter of text sequencially
-test.only('Press: Sequentially Method', async({page})=>{
+//press sequentially - enters each letter of text sequencially (mimicking key presses)
+test/*.only*/('Press: Sequentially Method', async({page})=>{
     await page.goto('https://opensource-demo.orangehrmlive.com');
     await page.locator('input[placeholder="Username"]').pressSequentially('Admin');
     await page.locator('input[placeholder="Password"]').pressSequentially('admin123');
     ///await page.locator('input[placeholder="Password"]').press('Enter');
     await page.locator('button[type="submit"]').click();
+    await page.locator('.oxd-userdropdown-tab').click();
+    await page.locator('text=Logout').click();
+    
+    await page.close();
+});
+
+//press sequentially with delay - enters each letter of text sequencially, then wait x mill secs
+test.only('Press: Sequentially Method with Delay', async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.locator('input[placeholder="Username"]').pressSequentially('Admin',{delay:200});
+    await page.locator('input[placeholder="Password"]').pressSequentially('admin123',{delay:200});
+    await page.locator('input[placeholder="Password"]').press('Enter'); //asking for the enter key to be pressed (so skipping the submit button)++++++
+    //await page.locator('button[type="submit"]').click(); //skipped due to above
     await page.locator('.oxd-userdropdown-tab').click();
     await page.locator('text=Logout').click();
     
