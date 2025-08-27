e866c3fb78324b0d6b0ee08f4b07ff8ae4d9fba2
jasonthorne <jasonthorne@live.ie>
2025-08-27 18:13:04 +0100

    press sequentially

--- 4_playwright/tests/textBoxHandling.spec.ts
+++ 4_playwright/tests/textBoxHandling.spec.ts
@@ -1,11 +1,11 @@
 import {test} from '@playwright/test';
 
 //Fill method - most common method for entering text into a text box:
-test('Fill Method', async({page})=>{
+//puts text immediately into text box
+test/*.only*/('Fill Method', async({page})=>{
     await page.goto('https://opensource-demo.orangehrmlive.com');
     await page.locator('input[placeholder="Username"]').fill('Admin');
     await page.locator('input[placeholder="Password"]').fill('admin123');
-    ///await page.locator('input[placeholder="Password"]').press('Enter');
     await page.locator('button[type="submit"]').click();
     await page.locator('.oxd-userdropdown-tab').click();
     await page.locator('text=Logout').click();
@@ -14,10 +14,11 @@ test('Fill Method', async({page})=>{
 });
 
 
-test('Press: Sequentially Method', async({page})=>{
+//press sequentially - enters each letter of text sequencially
+test.only('Press: Sequentially Method', async({page})=>{
     await page.goto('https://opensource-demo.orangehrmlive.com');
-    await page.locator('input[placeholder="Username"]').fill('Admin');
-    await page.locator('input[placeholder="Password"]').fill('admin123');
+    await page.locator('input[placeholder="Username"]').pressSequentially('Admin');
+    await page.locator('input[placeholder="Password"]').pressSequentially('admin123');
     ///await page.locator('input[placeholder="Password"]').press('Enter');
     await page.locator('button[type="submit"]').click();
     await page.locator('.oxd-userdropdown-tab').click();
