8a0261bbb433215d901678dfe45366bb2616a117
jasonthorne <jasonthorne@live.ie>
2025-08-28 20:40:56 +0100

    btn clicks

--- 4_playwright/tests/buttonHandling.spec.ts
+++ 4_playwright/tests/buttonHandling.spec.ts
@@ -1,7 +1,23 @@
 import {test, expect} from '@playwright/test';
+import { assert } from 'console';
 
-test('Regular Button Songle Click', async({page})=>{
+test/*.only*/('Regular Button Single Click', async({page})=>{
     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('#click_area').click(); //single click in click area
+    await expect(page.locator('#click_type')).toHaveText('Click');
+    await page.close();
+});
 
+test/*.only*/('Double Click', async({page})=>{
+    await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('#click_area').dblclick(); //dbl click in click area
+    await expect(page.locator('#click_type')).toHaveText('Double-Click');
+    await page.close();
+});
+
+test.only('Right Click', async({page})=>{
+    await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('#click_area').click({button:'right'}) //right click in click area
+    await expect(page.locator('#click_type')).toHaveText('Right-Click');
     await page.close();
 });
