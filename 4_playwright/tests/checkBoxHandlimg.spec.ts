3a50f06522c672b25b2bdf5b81ecd603239ab8d7
jasonthorne <jasonthorne@live.ie>
2025-08-31 23:45:01 +0100

    checkbox handling

--- 4_playwright/tests/checkBoxHandlimg.spec.ts
+++ 4_playwright/tests/checkBoxHandlimg.spec.ts
@@ -6,8 +6,25 @@ test('Checkbox Button Handling', async ({page})=>{
     const moviesCheckBox = page.locator('#checkbox2');
     const hockeyCheckBox = page.locator('#checkbox3');
 
-    
+    //on ititial load, all should be unchecked:
+    //1st way to assert:
+    expect(cricketCheckBox).not.toBeChecked();
+    expect(moviesCheckBox).not.toBeChecked();
+    expect(hockeyCheckBox).not.toBeChecked();
+    //2nd way to assert:
+    expect(await cricketCheckBox.isChecked()).toBeFalsy();
+    expect(await moviesCheckBox.isChecked()).toBeFalsy();
+    expect(await hockeyCheckBox.isChecked()).toBeFalsy();
 
+    //check all boxes:
+    cricketCheckBox.check();
+    moviesCheckBox.check();
+    hockeyCheckBox.check();
+
+    //assert that they are now checked:
+    expect(await cricketCheckBox.isChecked()).toBeTruthy();
+    expect(await moviesCheckBox.isChecked()).toBeTruthy();
+    expect(await hockeyCheckBox.isChecked()).toBeTruthy();
 
     await page.close();
 });
