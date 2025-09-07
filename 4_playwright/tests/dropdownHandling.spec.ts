8b4f6602b8ce75976b11b6f9d16adc910ab47f12
jasonthorne <jasonthorne@live.ie>
2025-09-07 17:43:44 +0100

    drop down handling

--- 4_playwright/tests/dropdownHandling.spec.ts
+++ 4_playwright/tests/dropdownHandling.spec.ts
@@ -1,2 +1,32 @@
 import {test} from '@playwright/test';
 
+//+++++++++++ 3 types of dropdowns: ++++++++++++++++
+//single static drop down (values dont change)
+//multi static dropdown (values dont change)
+//dynamic dropdown
+
+test('Single static dropdown handling', async({page})=>{
+    await page.goto('https://demo.automationtesting.in/Register.html');
+
+    //-------------------------------------------------------------------------------------------
+    //WAYS TO GET A SELECTION OPTION +++++++++++ specifically used for static dropdowns (where values are static)
+    //getting selection option by id and value:
+    await page.selectOption('#skills', {value: 'Android'});
+
+    await page.pause(); //pause page to show selection
+
+    //getting selection option by id and label (the text given for the option):
+    await page.selectOption('#skills', {label: 'Art Design'});
+
+    await page.pause(); //pause page to show selection
+
+    //getting selection option by id and index (option's position in list):
+    await page.selectOption('#skills', {index: 3});
+
+    await page.pause(); //pause page to show selection
+    //-------------------------------------------------------------------------------------------
+
+
+
+    await page.close();
+});
