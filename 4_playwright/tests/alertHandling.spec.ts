ed738d791fd3c1dd39b9c5a55db9f80955ea3b41
jasonthorne <jasonthorne@live.ie>
2025-09-24 23:43:30 +0100

    alert handling

---
+++ 4_playwright/tests/alertHandling.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+//+++++++++++++++NOTE: Playwright will automatically close the slert, so that doesnt need handling in tests 
+
+test('Simple Alert Handling', async({page})=>{
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    await page.locator("button[onclick='jsAlert()']").click(); //click on alert btn
+    await expect(page.locator('#result')).toHaveText('You successfully clicked an alert'); //confirm text
+
+
+    await page.close();
+});
