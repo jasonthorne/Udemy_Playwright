dfa64ce475630fafefb2b2b77595461449e7f2a7
jasonthorne <jasonthorne@live.ie>
2025-09-25 22:35:47 +0100

    alert handling

--- 4_playwright/tests/alertHandling.spec.ts
+++ 4_playwright/tests/alertHandling.spec.ts
@@ -6,7 +6,6 @@ test('Simple Alert Handling', async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
     await page.locator("button[onclick='jsAlert()']").click(); //click on alert btn
     await expect(page.locator('#result')).toHaveText('You successfully clicked an alert'); //confirm text
-
-
     await page.close();
 });
+
