1e7b643acfe2ebf554e3f1076b3fa0a35efd06d4
jasonthorne <jasonthorne@live.ie>
2025-10-09 23:43:23 +0100

    web table handling

--- 4_playwright/tests/webTableHandling.spec.ts
+++ 4_playwright/tests/webTableHandling.spec.ts
@@ -2,6 +2,21 @@ import {test, expect} from '@playwright/test';
 
 test.only("Handling Webtable", async({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/');
+    const table = page.locator("table[name='BookTable']"); //grab table locator
+
+    //=========grab total rows and colums of table:============
+
+    //colums:
+    //number of elements (<td> or <th>) in a <tr>, provide number of colums in a table:
+    const cols = table.locator('tr th'); //grab all <th> (table headers - one in each column) elements within a <tr>
+    console.log("Number of colums " + await cols.count());
+
+    //rows:
+    const rows = table.locator('tbody tr'); //get all tr elements within table body (includes <th>) 
+    console.log("Number of rows: " + await rows.count());
+
+    expect(await cols.count()).toBe(4); //check col count
+    expect(await rows.count()).toBe(7);//check row count
 
     await page.close();
 });
